cc77e3d9a3ca46ef41aa967ebf626b82c68650aa
CarlosFastLab <55218240+CarlosFastLab@users.noreply.github.com>
2020-09-11 12:55:31 -0300

    Add files via upload

---
+++ conversor.js
@@ -0,0 +1,12 @@
+function binToDec(bin) {
+    var bin = document.getElementById("campoBin").value;
+    let dec = 0;
+    console.log("Elemento capturado");
+    
+    for (let i = bin.length-1; i >= 0; i--) {
+        dec += parseInt(bin[i]) * Math.pow(2, bin.length-1-i);     
+    }
+
+    document.getElementById("result").value = dec;
+    return dec;
+}
