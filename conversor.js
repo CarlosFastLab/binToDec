8e9d5cabe5a1a036a8e966ba7067d280a7f3d007
CarlosFast <fastcaf@hotmail.com>
2020-09-17 10:28:39 -0300

    Finalizaçao do projeto com implementação de função para restringir caractéres digitáveis pelo usuário no campo de número Binário. Correções no formulário: ao submeter o form, a página não prossegue com refresh; enter também submete o formulário. Finalizações no CSS: botão com animação de transição de cor e alteração de cursor com hover.

--- conversor.js
+++ conversor.js
@@ -1,3 +1,15 @@
+function numCheck(event) {
+    var n = event.keyCode || event.which;
+    if((n != 48) && (n != 49) && (n != 13)) {
+        event.preventDefault();
+        window.alert("Por favor use apenas os digitos 1 e 0 para este campo")
+        return false
+    } else {
+        return true
+    }
+}
+
+
 function binToDec(bin) {
     bin = document.getElementById("campoBin").value;
     let dec = 0;
